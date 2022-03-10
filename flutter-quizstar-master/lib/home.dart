71c239fc06b86643e83ba5c2e0c80a7476f2fecb
vuquangngoc <vuquangngocnamdinh@gmail.com>
2022-03-10 18:24:31 +0700

    .

---
+++ flutter-quizstar-master/lib/home.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quizstar/quizpage.dart';
+
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+
+  List<String> images = [
+    "images/toan.png",
+    "images/vatly.png",
+    "images/hoahoc.png",
+    "images/tienganh.png",
+    "images/sinhhoc.png",
+  ];
+
+  List<String> des = [
+    "Để tăng tốc khả năng tính toán , hãy chuẩn bị sẵn máy tính và giấy bút cùng một cái đầu lạnh.\nLưu ý: Mỗi câu hỏi chỉ có 30 giây nên hãy cố gắng !",
+    "Java has always been one of the best choices for Enterprise World. If you think you have learn the Language...\nJust Test Yourself !!",
+    "Javascript is one of the most Popular programming language supporting the Web.\nIt has a wide range of Libraries making it Very Powerful !",
+    "C++, being a statically typed programming language is very powerful and Fast.\nit's DMA feature makes it more useful. !",
+    "Linux is a OPEN SOURCE Operating System which powers many Servers and Workstation.\nIt is also a top Priority in Developement Work !",
+  ];
+
+  Widget customcard(String langname, String image, String des){
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 30.0,
+      ),
+      child: InkWell(
+        onTap: (){
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            // in changelog 1 we will pass the langname name to ther other widget class
+            // this name will be used to open a particular JSON file 
+            // for a particular language
+            builder: (context) => getjson(langname),
+          ));
+        },
+        child: Material(
+          color: Colors.indigoAccent,
+          elevation: 10.0,
+          borderRadius: BorderRadius.circular(25.0),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                  ),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(100.0),
+                    child: Container(
+                      // changing from 200 to 150 as to look better
+                      height: 150.0,
+                      width: 150.0,
+                      child: ClipOval(
+                        child: Image(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            image,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    langname,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontFamily: "Quando",
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    des,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontFamily: "Alike"
+                    ),
+                    maxLines: 5,
+                    textAlign: TextAlign.justify,
+                  ),
+                  
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown, DeviceOrientation.portraitUp
+    ]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ÔN LUYỆN THI THPT QUỐC GIA",
+          style: TextStyle(
+            fontFamily: "Quando",
+            fontSize: 20
+          ),
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          customcard("Toán học", images[0], des[0]),
+          customcard("Vật lý", images[1], des[1]),
+          customcard("Hóa học", images[2], des[2]),
+          customcard("Tiếng Anh", images[3], des[3]),
+          customcard("Sinh học", images[4], des[4]),
+        ],
+      ),
+    );
+  }
+}
